b53e668b6834556bdf447625215bdfeab855e5d7
Manuel Sabin <msabin27@gmail.com>
2023-08-15 13:02:40 -0700

    Drawing working! Wave is still inverted wrt y-axis since origin is top-left, but wave draws correctly.  Starting with sound.  Looks like we have an oscillator making the default A4 tone.

--- script.js
+++ script.js
@@ -1,31 +1,63 @@
 const WIDTH = 500
 const HEIGHT = 500
+const NEON_PINK = [255, 16, 240]
+const NEON_BLUE = [4,217,255]
 
 
 let isClicked = false
-let wave = new Array(WIDTH).fill(250)
+let wave = new Array(WIDTH).fill(HEIGHT/2)
+
+let lastX = HEIGHT/2
+let lastY = HEIGHT/2
+
+let context
+
 
 function setup() {
-  console.log("hello!")
 
   createCanvas(500, 500)
   background(0, 0, 0)
+
+  context = new AudioContext()
+
+  osc = new OscillatorNode(context)
+  osc.connect(context.destination)
+  osc.type = "sawtooth"
+  osc.start()
 }
 
 function draw() {
-  console.log("test!")
   
+  background(0, 0, 0)
 
   for(let i = 0; i < wave.length-2; i++){
     line(i, wave[i], i+1, wave[i+1])
 
-    stroke(255,255,255)
+    stroke(NEON_BLUE)
+    strokeWeight(2)
   }
 }
 
 
 
 function mouseDragged(){
-  wave[mouseX] = mouseY
-  console.log({mouseX})
+
+  context.resume() // Keep the sound playing
+
+  offsetX = mouseX - lastX
+
+
+  // Fill sound wave array by interpolating between current mouse 
+  // position and previous mouse position.
+  length = Math.abs(offsetX)
+  sign = Math.sign(offsetX)
+
+  for(let i = 0; i < length; i++){
+    t = i/length // Interpolate t fraction between points
+
+    wave[lastX + sign*i] = lastY * (1 - t) + mouseY * t
+  }
+
+  lastX = mouseX
+  lastY = mouseY
 }
