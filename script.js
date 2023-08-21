bca38b8ccbf815eb78cd26c37d6e1a64e7069e51
Manuel Sabin <msabin27@gmail.com>
2023-08-21 16:12:37 -0700

    Added functionality to shift pitch with arrows (and reset the wave with the 'r' key).  Minor comments making clear what this code is doing.  Can shift across semi-tones and even down into the realm of rhythms with the drawn waveformgit add .

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ let context
 let osc
 let period
 
+let Hz = 220
+
 
 function setup() {
 
@@ -30,83 +32,9 @@ function setup() {
   osc = context.createOscillator()
   osc.connect(context.destination)
 
-  osc.frequency.setValueAtTime(220, context.currentTime)
+  osc.frequency.setValueAtTime(Hz, context.currentTime)
   
   osc.start()
-
-  // Hz = 1
-  // cycles = 1
-  // print(context.sampleRate)
-  // let pcm = new Float32Array(context.sampleRate/Hz)    //(cycles*context.sampleRate)
-
-  // for(let i = 0; i < pcm.length; i++){
-  //   pcm[i] = Math.cos(2*Hz * 2 * Math.PI * i * (cycles/pcm.length))
-
-  //   pcm[i] += Math.cos(3*Hz * 2 * Math.PI * i * (cycles/pcm.length))
-  // }
-
-  // let real = pcm.slice()
-  // let imag = new Float32Array(pcm.length)
-
-  // transform(real, imag)
-
-  // print(real, imag)
-
-  // let period = context.createPeriodicWave(real, imag)
-
-  // let osc = context.createOscillator()
-
-  // osc.setPeriodicWave(period)
-
-  // osc.frequency.setValueAtTime(220, context.currentTime)
-
-  // osc.connect(context.destination)
-
-  // osc.start()
-
-
-  // const buffer = context.createBuffer(
-  //   1, 
-  //   cycles * context.sampleRate, 
-  //   context.sampleRate
-  //   )
-
-  // buffer.copyToChannel(pcm, 0)
-
-  // const bufferNode = context.createBufferSource()
-
-  // bufferNode.buffer = buffer
-
-
-  // const analyser = context.createAnalyser()
-  // bufferNode.connect(analyser)
-  
-  // const spectrum = new Float32Array(buffer.length)
-  // analyser.getFloatFrequencyData(spectrum)
-  
-
-
-  // bufferNode.connect(context.destination)
-
-  // bufferNode.start()
-
-  // osc = new OscillatorNode(context)
-  // osc.connect(context.destination)
-
-  // const real = new Float32Array(7);
-  // const imag = new Float32Array(7);
-  
-  
-  // const sine = context.createPeriodicWave(real, imag);
-  
-  // osc.setPeriodicWave(sine);
-  
-  
-
-  
-  // osc.type = "sawtooth"
-  // osc.start()
-  // osc.stop(2)
 }
 
 function draw() {
@@ -122,6 +50,31 @@ function draw() {
 }
 
 
+function keyPressed(){
+  // Shift pitch by semitone with the arrows
+
+  if(keyCode === LEFT_ARROW){
+    Hz /= 2 ** (1/12)
+    console.log("left")
+    osc.frequency.setValueAtTime(Hz, context.currentTime)
+  }
+  if(keyCode === RIGHT_ARROW){
+    Hz *= 2 ** (1/12)
+    console.log("right")
+    osc.frequency.setValueAtTime(Hz, context.currentTime)
+  }
+}
+
+function keyTyped() {
+  if (key === 'r') {
+    screenWave.fill(HEIGHT/2)
+    pcm.fill(0)
+    period = context.createPeriodicWave(pcm, pcm)
+    osc.setPeriodicWave(period)
+  }
+
+}
+
 
 function mouseDragged(){
 
@@ -135,34 +88,30 @@ function mouseDragged(){
   length = Math.abs(offsetX)
   sign = Math.sign(offsetX)
 
-  console.log(mouseX, lastX)
-
   if(Math.floor(length) === 0){
-    return
+    return  // Don't want to divide by zero.
   }
 
-
   for(let i = 0; i < length; i++){
-    t = i/length // Interpolate t fraction between points
+    t = i/length // Interpolate t fraction between points.
 
     screenWave[lastX + sign*i] = lastY * (1 - t) + mouseY * t
-    console.log(screenWave[lastX + sign*i])
 
+    // Normalize the screen's wave to be PCM samples in [-1, 1].
     pcm[lastX + sign*i] = -(screenWave[lastX + sign*i]-HEIGHT/2)/(HEIGHT/2)
-    console.log(pcm[lastX + sign*i])
   }
 
   lastX = mouseX
   lastY = mouseY
 
+  // Time domain real and imag.
   real = pcm.slice()
   imag = imag.fill(0)
 
-  console.log(real, imag)
+  // Use FFT to fill real and imag with frequency domain.
   transform(real, imag)
 
-  console.log(real, imag)
+  // Create a PeriodicWave object with the spectrum.
   period = context.createPeriodicWave(real, imag)
-
   osc.setPeriodicWave(period)
 }
