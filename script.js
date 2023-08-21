8deb3421e095e040a2d7c48fba758c34c8b9222b
Manuel Sabin <msabin27@gmail.com>
2023-08-21 14:41:51 -0700

    Complete idea is workinggit add .  Screen wave is normalized into a pcm, which is then frequency analyzed to create a PeriodicWave object that the oscillator uses to loop and I can change frequencies at will.  Going to add some functionality now so that I can have buttons change the frequencies.

--- script.js
+++ script.js
@@ -1,53 +1,68 @@
-const WIDTH = 500
-const HEIGHT = 500
+const WIDTH = 512
+const HEIGHT = 512
 const NEON_PINK = [255, 16, 240]
 const NEON_BLUE = [4,217,255]
 
 
 let isClicked = false
+
 let screenWave = new Array(WIDTH).fill(HEIGHT/2)
+let pcm = new Float32Array(screenWave.length)
+let real = pcm.slice()
+let imag = new Float32Array(pcm.length)
+
 
 let lastX = HEIGHT/2
 let lastY = HEIGHT/2
 
 let context
+let osc
+let period
 
 
 function setup() {
 
-  createCanvas(500, 500)
+  createCanvas(WIDTH, HEIGHT)
   background(0, 0, 0)
 
   context = new AudioContext()
 
-
-
-  Hz = 440
-  cycles = 1
-  let pcm = new Float32Array(context.sampleRate/Hz)    //(cycles*context.sampleRate)
-
-  for(let i = 0; i < pcm.length; i++){
-    pcm[i] = .5*Math.sin(Hz * 2 * Math.PI * i * (cycles/pcm.length))
-  }
-
-  let real = pcm.slice()
-  let imag = new Float32Array(pcm.length)
-
-  transform(real, imag)
-
-  
-
-  let period = context.createPeriodicWave(real, imag)
-
-  let osc = context.createOscillator()
-
-  osc.setPeriodicWave(period)
-
-  osc.frequency.setValueAtTime(75, context.currentTime)
-
+  osc = context.createOscillator()
   osc.connect(context.destination)
 
+  osc.frequency.setValueAtTime(220, context.currentTime)
+  
   osc.start()
+
+  // Hz = 1
+  // cycles = 1
+  // print(context.sampleRate)
+  // let pcm = new Float32Array(context.sampleRate/Hz)    //(cycles*context.sampleRate)
+
+  // for(let i = 0; i < pcm.length; i++){
+  //   pcm[i] = Math.cos(2*Hz * 2 * Math.PI * i * (cycles/pcm.length))
+
+  //   pcm[i] += Math.cos(3*Hz * 2 * Math.PI * i * (cycles/pcm.length))
+  // }
+
+  // let real = pcm.slice()
+  // let imag = new Float32Array(pcm.length)
+
+  // transform(real, imag)
+
+  // print(real, imag)
+
+  // let period = context.createPeriodicWave(real, imag)
+
+  // let osc = context.createOscillator()
+
+  // osc.setPeriodicWave(period)
+
+  // osc.frequency.setValueAtTime(220, context.currentTime)
+
+  // osc.connect(context.destination)
+
+  // osc.start()
 
 
   // const buffer = context.createBuffer(
@@ -85,7 +100,6 @@ function setup() {
   // const sine = context.createPeriodicWave(real, imag);
   
   // osc.setPeriodicWave(sine);
-  // osc.frequency.setValueAtTime(220, context.currentTime)
   
   
 
@@ -121,12 +135,34 @@ function mouseDragged(){
   length = Math.abs(offsetX)
   sign = Math.sign(offsetX)
 
+  console.log(mouseX, lastX)
+
+  if(Math.floor(length) === 0){
+    return
+  }
+
+
   for(let i = 0; i < length; i++){
     t = i/length // Interpolate t fraction between points
 
     screenWave[lastX + sign*i] = lastY * (1 - t) + mouseY * t
+    console.log(screenWave[lastX + sign*i])
+
+    pcm[lastX + sign*i] = -(screenWave[lastX + sign*i]-HEIGHT/2)/(HEIGHT/2)
+    console.log(pcm[lastX + sign*i])
   }
 
   lastX = mouseX
   lastY = mouseY
+
+  real = pcm.slice()
+  imag = imag.fill(0)
+
+  console.log(real, imag)
+  transform(real, imag)
+
+  console.log(real, imag)
+  period = context.createPeriodicWave(real, imag)
+
+  osc.setPeriodicWave(period)
 }
