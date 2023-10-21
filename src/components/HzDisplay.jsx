4449c151e73b39e6edb122f051d8ed8931ce7ec6
Manuel Sabin <msabin27@gmail.com>
2023-10-20 18:01:02 -0700

    Put some limits on how much can be typed into the Hz display and default to previous Hz on invalid inputs.  Looks good to go for something that works and looks decent!

--- src/components/HzDisplay.jsx
+++ src/components/HzDisplay.jsx
@@ -1,19 +1,48 @@
 export function HzDisplay ( { Hz, onChangeHz } ) {
   const SEMITONE_FACTOR = 2 ** (1 / 12);
-  console.log(Hz)
+  const MAX_CHARACTER_LIMIT = 8;
 
-  function handleBlur (newHz) {
+  function handleBlur (e) {
+    let newHz = e.currentTarget.textContent || ''
+
     // If the entered value is empty or not a number, keep old Hz
     if (!newHz.trim() || isNaN(newHz)) {
-      newHz = Hz;
+      (e.target).textContent = Hz.toFixed(2);
     }
-    onChangeHz(parseFloat(newHz))
+    else if (newHz > 24000) {newHz = 24000}
+    else if (newHz < -24000) {newHz = -24000}
+    
+    onChangeHz(parseFloat(newHz)) 
   };
 
   function handleItemKeyDown (e) {
     if (e.key === 'Enter') {
       e.preventDefault();
       e.currentTarget.blur();
+    }
+  };
+
+  const handleInput = (e) => {
+    const text = (e.target).textContent || '';
+    
+    if (text.length > MAX_CHARACTER_LIMIT) {
+      // Prevent entering more characters than the limit
+      e.preventDefault();
+      const truncatedText = text.slice(0, MAX_CHARACTER_LIMIT);
+      
+      // Restore cursor position after a small delay
+      setTimeout(() => {
+        const range = document.createRange();
+        const selection = window.getSelection();
+        if (selection) {
+          range.setStart((e.target).childNodes[0], truncatedText.length);
+          range.collapse(true);
+          selection.removeAllRanges();
+          selection.addRange(range);
+        }
+      }, 0);
+
+      (e.target).textContent = truncatedText;
     }
   };
 
@@ -26,8 +55,10 @@ export function HzDisplay ( { Hz, onChangeHz } ) {
         className="digital-display"
         contentEditable={true}
         suppressContentEditableWarning={true}
-        onBlur={(e) => handleBlur(e.currentTarget.textContent || '')}
+        onBlur={(e) => handleBlur(e)}
         onKeyDown={(e) => handleItemKeyDown(e)}
+        onInput={(e) => handleInput(e)}
+        style={{width: "7em"}}
       > 
         {Hz.toFixed(2)} 
       </p>
